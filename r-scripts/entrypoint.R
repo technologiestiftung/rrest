c21b5f37e4b382047b8f29b98e472fbfc67c95ef
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-01-21 15:23:36 +0100

    feat(r from stdin): can read data that was piped in

--- r-scripts/entrypoint.R
+++ r-scripts/entrypoint.R
@@ -2,9 +2,13 @@
 setwd('./r-scripts')
 source(".Rprofile")
 library('jsonlite')
-Sys.sleep(0.5)
-data = data.frame(
-   id = c(1:5),
-  names = c("foo", "bah", "baz","boom","bang")
-)
-toJSON(data)
+
+main <- function () {
+  con <-  file("stdin")
+  lines <- readLines(con)
+  close(con)
+  Sys.sleep(0.5)
+  toJSON(lines)
+}
+
+main()
