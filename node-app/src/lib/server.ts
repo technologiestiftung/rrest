cd52816d0e40ec685bf2abea9dfb247bb55a6b4d
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-01-28 16:22:37 +0100

    feat(CORS): add CORS middleware

--- node-app/src/lib/server.ts
+++ node-app/src/lib/server.ts
@@ -1,6 +1,8 @@
+import cors from '@koa/cors';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import json from 'koa-json';
+import logger from 'koa-logger';
 import mount from 'koa-mount';
 // import Router from 'koa-router';
 import serve from 'koa-static';
@@ -12,13 +14,23 @@ const app = new Koa();
 
 pub.use(serve(path.resolve(process.cwd(), './node-app/public')));
 api.use(json({ pretty: false, param: 'pretty' }));
+api.use(logger());
+api.use(cors({origin: 'http://localhost:1234'}));
 api.use(bodyParser());
 api.use(async (ctx: Koa.Context) => {
     const response = await rexecute('./r-scripts/entrypoint.R', ctx.request.body);
     // if (response instanceof Error) {
     //   throw response;
     // } else {
-    ctx.body = JSON.stringify(response);
+    // tslint:disable-next-line:no-console
+    console.log(response);
+    if (response.code === 0) {
+      ctx.status = 200;
+      ctx.body = response;
+    } else {
+      ctx.status = 500;
+      ctx.body = response.errors;
+    }
     // }
 // process.stdout.write();
 // console.log(JSON.stringify(ctx.request.body));
